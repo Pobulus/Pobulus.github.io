224bdb29a6cedc480d4d1659610adeb23ce1741e
Pobulus <32367046+Pobulus@users.noreply.github.com>
2020-11-03 23:39:22 +0100

    Split vngine to css and js files
    also script changed to new experimental format

---
+++ vngine-code.js
@@ -0,0 +1,66 @@
+var elem=document.documentElement;
+var fullscr=false; 
+/* View in fullscreen */
+function openFullscreen() {
+    if (!fullscr){
+        fullscr=true;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }}
+    else{
+        fullscr=false;
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+        }
+}
+var mainCounter = 0;
+var textLines = new Array;
+function load(){
+    $.get('https://pobulus.github.io/script.txt', function(data){
+        console.log(data);
+        textLines = data.split('\n');
+        console.log(textLines);
+        readLine(mainCounter);
+    });
+}
+
+function nextLine(){
+    if (mainCounter<textLines.length-1){
+        mainCounter = mainCounter+1;
+    } else {
+        console.log("End of the script!");
+    }
+    
+    readLine(mainCounter);
+}
+function prevLine(){
+    if (!mainCounter==0){
+        mainCounter = mainCounter-1;
+    } else {
+        console.log("Start of the script!");
+    }
+    
+    readLine(mainCounter);
+}
+function readLine(x){
+    
+    textLineArgs = textLines[x].split("; ");
+    console.log(textLineArgs);
+    display(textLineArgs[1]);
+}
+function display(x){
+    $("#textDisplay").text(x);
+    console.log(x);
+}
+function changeBackground(x){
+    $("#backdrop").attr('src', x);
+}
