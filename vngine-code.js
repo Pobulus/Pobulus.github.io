6942f0e5bf5813b3f70aa9be4b9af5d046b7e543
Pobulus <32367046+Pobulus@users.noreply.github.com>
2020-11-06 01:32:26 +0100

    testing multiple actors
    updated script again

--- vngine-code.js
+++ vngine-code.js
@@ -1,6 +1,6 @@
 var elem=document.documentElement;
-var names = {"s":"Simp","c":"Chad"};
-var colors = {"s": "#00ff00", "c":"#00ffff"};
+var names = {"s":"Simp","c":"Chad","b":"Bob"};
+var colors = {"s": "#00ff00", "c":"#00ffff","b":"#ff8800"};
 var fullscr=false; 
 /* View in fullscreen */
 function openFullscreen() {
