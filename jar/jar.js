e37517ab5b4d5c5f2dadeaa6e5e84bd67adbe14e
Pobulus <32367046+Pobulus@users.noreply.github.com>
2022-02-21 23:14:11 +0100

    text test

--- jar/jar.js
+++ jar/jar.js
@@ -5,14 +5,25 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
   
+
+function scriptLoad(data){
+    console.log(data);
+    textLines = data.split('\n');
+    return textLines;
+}
 var colors = ["chall", "coup", "nice"]
 function main(){
+    
+    $.get('https://pobulus.github.io/jar/cards.txt', function(data){
+        var cards = scriptLoad(data);
+    });
+    $("#card").text(cards[getRandomInt(0, cards.length)]);
     $("#curtain").remove();
     console.log("started");
     for(i =0; i<100;i++){
         $("#jar-container").prepend("<div class=\" "+colors[getRandomInt(0, colors.length)]+ " others\" style=\"transform: rotate("+getRandomInt(-180, 180)+"deg);left: "+getRandomInt(4, 14)+"vmin;\"></div>");
     }
     $("#card").addClass(colors[getRandomInt(0, colors.length)]);
-    $("#card").text("");
+    
     $("#card").css("animation", "pickout 3s");
 }
