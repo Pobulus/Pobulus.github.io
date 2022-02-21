0dcb5df57fe5c84daed10b806814d551e89c6450
Pobulus <32367046+Pobulus@users.noreply.github.com>
2022-02-21 23:00:42 +0100

    loading improvements

--- jar/jar.js
+++ jar/jar.js
@@ -1,7 +1,18 @@
 $(document).ready(main);
-
-
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+var colors = ["chall", "coup", "nice"]
 function main(){
+    $("#curtain").remove();
     console.log("started");
+    for(i =0; i<100;i++){
+        $("#jar-container").prepend("<div class=\" "+colors[getRandomInt(0, colors.length)]+ " others\" style=\"transform: rotate("+getRandomInt(-180, 180)+"deg);left: "+getRandomInt(4, 14)+"vmin;\"></div>");
+    }
+    $("#card").addClass(colors[getRandomInt(0, colors.length)]);
+    $("#card").text("");
     $("#card").css("animation", "pickout 3s");
 }
